9cdc969ae93f268cc8f037f16b88ab37792bb32a
sedna08 <ochiamitcham@gmail.com>
2024-10-07 23:39:55 +0800

    Created gitignore, docker-compose for the database, installed some other dependencies and updated index.ts for the backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,37 @@
+import * as express from 'express';
+import "reflect-metadata"
+import * as dotenv from "dotenv";
+import * as cors from 'cors';
 import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import { Express } from "express";
 
-AppDataSource.initialize().then(async () => {
+dotenv.config();
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
+const PORT = parseInt(process.env.SERVER_PORT,10);
+const HOST = '0.0.0.0';
 
-    console.log("Here you can setup and run express / fastify / any other framework.")
+AppDataSource.initialize()
+    .then(async () => {
+        console.log("Data Source has been initialized!");
+        
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization:", err)
+    });
 
-}).catch(error => console.log(error))
+
+// create and setup express app
+const app: Express = express();
+app.use(express.json());
+app.use(cors({
+    origin: [process.env.FRONTEND_ORIGIN],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+}));
+// app.use(parcelRoutes);
+
+// start express server
+app.listen(PORT, HOST, () => {
+    console.log(`Server is running and listening at ${PORT}`);
+})
