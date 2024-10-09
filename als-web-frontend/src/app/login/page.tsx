a19298765a4d8a059429c12ba8de563d8c926dc1
sedna08 <ochiamitcham@gmail.com>
2024-10-09 10:39:40 +0800

    Added Header component, and created initial login page

--- als-web-frontend/src/app/login/page.tsx
+++ als-web-frontend/src/app/login/page.tsx
@@ -1,7 +1,13 @@
+import Header from "@/components/Headers"
+
 export default function Login() {
     return (
         <div>
-            <h1>Login Page</h1>
+            <Header/>
+            <main className="flex flex-col items-center min-h-screen bg-gray-100">
+                <h1>Login Page</h1>
+            </main>
+            
         </div>
     )
 }
