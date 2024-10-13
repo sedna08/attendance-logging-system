9159683c148033d57d6f2e319666be14169a807f
sedna08 <ochiamitcham@gmail.com>
2024-10-13 18:32:54 +0800

    Initialized apiGateway directory

---
+++ apigateway/server.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+const port = 3000;
+
+
+app.get('/hello', (req, resp) => {
+    return resp.send('HELLO WORLD!');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
