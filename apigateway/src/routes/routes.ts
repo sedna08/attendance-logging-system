23043bf2c9a93e3ad4c399e5741e944eae74860d
sedna08 <ochiamitcham@gmail.com>
2024-10-13 23:30:25 +0800

    Updated routes.ts to handle rotues in a more manageable manner

--- apigateway/src/routes/routes.ts
+++ apigateway/src/routes/routes.ts
@@ -1,48 +1,45 @@
 import { Route } from "../utils/types";
 import * as dotenv from "dotenv";
 
-
 dotenv.config();
 
+const BACKEND_URL = process.env.BACKEND_SERVER_URL;
+
+if (!BACKEND_URL) {
+    console.warn("Warning: BACKEND_SERVER_URL environment variable is not set.");
+    throw new Error("BACKEND_SERVER_URL environment variable is not set.");
+}
+
+// Enum to manage route URLs
+enum UserServiceRoutes {
+    Users = '/api/userService/users',
+    Signup = '/api/userService/signup',
+    Login = '/api/userService/login',
+    UserById = '/api/userService/users/:id',
+    Logout = '/api/userService/logout'
+}
+
+/**
+ * Creates a route object for the proxy middleware.
+ * @param url - The URL for the route.
+ * @param auth - Whether authentication is required for this route.
+ * @returns A route object.
+ */
+const createRoute = (url: string, auth: boolean = false): Route => ({
+    url,
+    auth,
+    proxy: {
+        // Remove '/api' from the target URL
+        target: `${BACKEND_URL}${url.replace('/api', '')}`,
+        changeOrigin: true,
+    },
+});
+
+// Exporting routes
 export const ROUTES: Route[] = [
-    {
-        url: '/api/userService/users',
-        auth: false,
-        proxy: {
-            target: `${process.env.BACKEND_SERVER_URL}/userService/users`,
-            changeOrigin: true,
-        }
-    },
-    {
-        url: '/api/userService/signup',
-        auth: false,
-        proxy: {
-            target: `${process.env.BACKEND_SERVER_URL}/userService/signup`,
-            changeOrigin: true,
-        }
-    },
-    {
-        url: '/api/userService/login',
-        auth: false,
-        proxy: {
-            target: `${process.env.BACKEND_SERVER_URL}/userService/login`,
-            changeOrigin: true,
-        }
-    },
-     {
-        url: '/api/userService/users/:id',
-        auth: false,
-        proxy: {
-            target: `${process.env.BACKEND_SERVER_URL}/userService/users/:id`,
-            changeOrigin: true,
-        }
-    },
-     {
-        url: '/api/userService/logout',
-        auth: false,
-        proxy: {
-            target: `${process.env.BACKEND_SERVER_URL}/userService/logout`,
-            changeOrigin: true,
-        }
-    },
+    createRoute(UserServiceRoutes.Users),
+    createRoute(UserServiceRoutes.Signup),
+    createRoute(UserServiceRoutes.Login),
+    createRoute(UserServiceRoutes.UserById),
+    createRoute(UserServiceRoutes.Logout),
 ];
